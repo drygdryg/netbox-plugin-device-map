44211d254a93e9ca49ac942692288d2a2c64e4af
Victor Golovanenko <drygdryg2014@yandex.com>
2022-08-26 15:06:56 +0300

    NetBox v3.3 support

--- netbox_device_map/helpers.py
+++ netbox_device_map/helpers.py
@@ -1,11 +1,15 @@
+from packaging import version
+
 from dcim.models import Device
 from django.db.models import QuerySet, Q
 from ipam.models import VLAN
+from netbox.settings import VERSION
 
 from .settings import plugin_settings
 
 
 LOCATION_CF_NAME = plugin_settings['device_geolocation_cf']
+NETBOX_VERSION = version.parse(VERSION)
 LatLon = tuple[float, float]
 
 
@@ -22,11 +26,19 @@ def get_connected_devices(device: Device, vlan: VLAN = None) -> QuerySet[Device]
     included_interfaces = device.interfaces.all()
     if vlan is not None:
         included_interfaces = included_interfaces.filter(Q(untagged_vlan=vlan) | Q(tagged_vlans=vlan))
-    return Device.objects.filter(interfaces___link_peer_id__in=included_interfaces)
+    if NETBOX_VERSION < version.parse('3.3.0'):
+        return Device.objects.filter(interfaces___link_peer_id__in=included_interfaces)
+    else:
+        return Device.objects.filter(
+            interfaces__cable__terminations__interface__in=device.interfaces.all()
+        ).exclude(pk=device.id)
 
 
 def are_devices_connected(device_a: Device, device_b: Device) -> bool:
     """Determines whether devices are connected to each other by a direct connection"""
-    return bool(
-        Device.objects.filter(interfaces___link_peer_id__in=device_a.interfaces.all(), id=device_b.id).values('pk')
-    )
+    if NETBOX_VERSION < version.parse('3.3.0'):
+        queryset = Device.objects.filter(interfaces___link_peer_id__in=device_a.interfaces.all(), id=device_b.id)
+    else:
+        queryset = Device.objects.filter(
+            interfaces__cable__terminations__interface__in=device_a.interfaces.all(), id=device_b.id)
+    return bool(queryset.values('pk'))
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='netbox-plugin-device-map',
-    version='0.1.0',
+    version='0.1.1',
     description='A simple device map with filter criteria',
     long_description=long_description,
     long_description_content_type='text/markdown',
